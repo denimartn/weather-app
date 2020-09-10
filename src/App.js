7893e72e6356e58437e199a9c69cbd27184eec7f
denimartn <43113570+denimartn@users.noreply.github.com>
2020-09-10 19:22:13 +0200

    add loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
 import "./tailwind.output.css";
 import axios from "axios";
@@ -13,7 +13,7 @@ function App() {
     setState("loading");
     try {
       const api =
-        "https://cors-anywhere.herokuap.com/https://www.metaweather.com/api";
+        "https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api";
       const query = inputValue;
       const res = await axios.get(`${api}/location/search/?query=${query}`);
       if (!res.data[0]) {
@@ -39,7 +39,7 @@ function App() {
   };
 
   return (
-    <div className="main-container w-full">
+    <div className="main-container w-full ">
       <h1 className="text-center m-4 font-bold text-4xl max-w-md mx-auto">
         #weather app
       </h1>
@@ -64,12 +64,34 @@ function App() {
         ) : null}
 
         <div className="container mx-auto px-2 flex justify-center">
-          {state === "loading" ? <p>Loading...</p> : null}
+          {state === "loading" ? (
+            <div className="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150 cursor-not-allowed">
+              <svg
+                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+              Processing
+            </div>
+          ) : null}
           {state === "error" ? (
             <div
-              className="bg-red-lightest border border-red-light text-red-dark pl-4 pr-8 py-3 rounded flex"
+              className="bg-red-100 border border-red-400 text-red pl-4 pr- py-3  px-3 rounded flex"
               role="alert"
             >
+              {/* add space */}
               <strong className="font-bold">Brbrbr! </strong>
               Something seriously went wrong.
               <span>
