d0862c7b633e560295689b1ff0f314139516748f
denise <dmartini742@gmail.com>
2020-09-23 12:06:02 +0200

    Updates

---
+++ precache-manifest.44b8edf5be9d30d63a05fc3204704f1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3ca8fbcafc696b1de3d699f2410ad6",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "080ef8225313837e250c",
+    "url": "/weather-app/static/css/main.34bfaf54.chunk.css"
+  },
+  {
+    "revision": "55aacd1c010bd352b230",
+    "url": "/weather-app/static/js/2.15ffeeac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.15ffeeac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "080ef8225313837e250c",
+    "url": "/weather-app/static/js/main.33e86e04.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
