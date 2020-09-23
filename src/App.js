942c068dda15c268ec03401055c42ceff001a516
denise <dmartini742@gmail.com>
2020-09-23 11:24:55 +0200

    update utils import

--- src/App.js
+++ src/App.js
@@ -77,27 +77,4 @@ function App() {
   );
 }
 
-// Utilities
-export function floor(n) {
-  return Math.floor(n);
-}
-
-export function dateConverter(str) {
-  let newStr = "";
-  const dateToCheck = new Date(str);
-  const today = new Date();
-  if (dateToCheck.toDateString() === today.toDateString()) {
-    newStr = "Today";
-  } else {
-    //format date
-    let options = {
-      weekday: "short",
-      month: "short",
-      day: "numeric",
-    };
-    newStr = new Intl.DateTimeFormat("en-US", options).format(dateToCheck);
-  }
-  return newStr;
-}
-
 export default App;
--- src/WeatherCards.js
+++ src/WeatherCards.js
@@ -1,24 +1,20 @@
 import React from "react";
-import {WeatherCard} from "./WeatherCard"
-import {dateConverter} from "./App"
-import {floor} from "./App"
-
+import { WeatherCard } from "./WeatherCard";
+import { dateConverter, floor } from "./utils";
 
 export const WeatherCards = (props) => {
-    console.log(props)
-    return (
-
-        <div className="cards px-4 py-4 flex justify-center">
-              {props.locationWeather.map((weather, index) => (
-               <WeatherCard
-               key={index}
-               date={dateConverter(weather.applicable_date)}
-               src={`https://www.metaweather.com/static/img/weather/${weather.weather_state_abbr}.svg`}
-               minTemp={floor(weather.min_temp)}
-               maxTemp={floor(weather.max_temp)}
-               description={weather.weather_state_name}
-               />
-              ))} 
-             </div>
-    ) 
-}
+  return (
+    <div className="cards px-4 py-4 flex justify-center">
+      {props.locationWeather.map((weather, index) => (
+        <WeatherCard
+          key={index}
+          date={dateConverter(weather.applicable_date)}
+          src={`https://www.metaweather.com/static/img/weather/${weather.weather_state_abbr}.svg`}
+          minTemp={floor(weather.min_temp)}
+          maxTemp={floor(weather.max_temp)}
+          description={weather.weather_state_name}
+        />
+      ))}
+    </div>
+  );
+};
